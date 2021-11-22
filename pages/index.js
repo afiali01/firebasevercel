cdc172e79c77addbbc442cf07e4613fa5afe34da
afiali01 <59668204+afiali01@users.noreply.github.com>
2021-11-22 09:52:02 -0700

    useState and hook lesson

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,49 @@
+import {useState} from 'react';
+import router, { useRouter } from 'next/router'
+import useSWR from 'swr'
+import styled from 'styled-components'
+import {Button} from 'ui/buttons';
+import {List, ListItem} from 'classcomp/present'
 
-import {GoogleProvider} from 'components/auth/providers/google'
+const Wrapper = styled.div`
+        padding: 6rem 0 0;
+        text-align: center;
+        button{
+            width: 40%;
+        }
+        p{
+            font-size: 4.25rem;
+        }
+`;
+
+//Dumb presentational
+//tempating render data+temp
+//smart logic
+//template render data+temp code listeners
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 
+function index(props) {
+    const [isValidUser, setValidUser] = useState(false)
+    const rounter = useRouter()
 
-function Home() {
+    function onRequestToSignIn(){
+        //firebase login logic returns a promise
+        //user, error
+        //user dashboard protected route
+        setValidUser(true)
+    
+    }
+
+    if(isValidUser){
+        router.push('http://www.apple.ca')
+    }
 
     return (
-        <div>
-            <GoogleProvider />
-        </div>
-    );
+        <Wrapper>
+            <Button onClick={onRequestToSignIn}>Login</Button>
+        </Wrapper>
+    )
 }
-
-export default Home
+export default index
