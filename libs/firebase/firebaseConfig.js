b66976900001dd156d51ea4d4153d3344b91001f
afiali01 <59668204+afiali01@users.noreply.github.com>
2021-11-25 10:00:21 -0700

    Added firebase

--- libs/firebase/firebaseConfig.js
+++ libs/firebase/firebaseConfig.js
@@ -2,9 +2,10 @@
 const firebaseConfig = {
     apiKey: "AIzaSyDcwgxPOuXP-4y75YiKuiLHQEBxKlm7fLY",
     authDomain: "dmit2008-47b17.firebaseapp.com",
+    databaseURL: "https://dmit2008-47b17-default-rtdb.firebaseio.com",
     projectId: "dmit2008-47b17",
     storageBucket: "dmit2008-47b17.appspot.com",
     messagingSenderId: "422106698477",
-    appId: "1:422106698477:web:9046130480686465b0f2e4",
-    measurementId: "G-PKQ3V182GJ"
+    appId: "1:422106698477:web:10dab6e1494b4670b0f2e4",
+    measurementId: "G-N5RWWQV8N1"
 };
